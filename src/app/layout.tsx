4225fea914573a55c3aab5f1974f9563a99f5ab2
Julian Salcedo <julian.salcedo@qubika.com>
2026-03-06 09:43:23 -0300

    Services texts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,11 +45,11 @@ export const metadata: Metadata = {
   publisher: 'Asegurate',
   icons: {
     icon: [
-      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-new-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-new-32x32.png', sizes: '32x32', type: 'image/png' },
     ],
     apple: [{ url: '/apple-touch-icon.png' }],
-    shortcut: '/favicon.ico',
+    shortcut: '/favicon-new.ico',
   },
   manifest: '/site.webmanifest',
   openGraph: {
